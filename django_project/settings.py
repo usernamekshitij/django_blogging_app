f133b63e06a32079ffbc242a8ac1093a18c273f3
usernamekshitij <usernamekshitij@gmail.com>
2019-06-01 16:35:31 +0530

    DATABASE

--- django_project/settings.py
+++ django_project/settings.py
@@ -61,7 +61,7 @@ import os.path
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'/template/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'usernamekshitij',
+        'USER':'usernamekshitij',
+        'PASSWORD':'#10kshitijpostgres',
+        'HOST':'localhost',
+        'PORT':'5432'
     }
 }
 
