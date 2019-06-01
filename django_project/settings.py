94a1e6270a64d1f361f606100d4b0b38dcb3f408
usernamekshitij <usernamekshitij@gmail.com>
2019-06-01 15:59:15 +0530

    second commit

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY=os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mydjangobloggingapp.herokuapp.com','localhost']
+ALLOWED_HOSTS = ['https://django-project-blogging-app.herokuapp.com/','localhost']
 
 
 # Application definition
